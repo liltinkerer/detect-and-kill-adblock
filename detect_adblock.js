3e98313970a715f7750cdd7203b817bae55592de
liltinkerer <liamenoc@gmail.com>
2016-09-13 16:34:57 +0200

    Update detect_adblock.js

--- detect_adblock.js
+++ detect_adblock.js
@@ -1,3 +1,31 @@
 (function() {
-	
+	document.addEventListener("DOMContentLoaded", function() {
+		// Create a "fake ad"
+		var fake_ad = document.createElement('div');
+		fake_ad.innerHTML = '&nbsp;';
+		// Hide it from viewport
+		fake_ad.setAttribute('style', 'top: -999px; position: fixed;');
+		// Add some Adblock bait classes
+		fake_ad.className = 'adsbox ad-300x250 banner-ad';
+
+		// Append the ad to DOM
+		document.body.appendChild(fake_ad);
+
+		// Wait 500ms for the adblocker to do its job 
+		window.setTimeout(function() {
+			if (fake_ad.offsetHeight === 0) {
+				// Add class .adb to body
+				document.body.classList.add('adb');
+				// For testing
+				console.log("Adblock is enabled!");
+				document.write("Adblock is enabled!");
+			} else {
+				// For testing
+				console.log("Adblock is disabled!");
+				document.write("Adblock is disabled!");
+			}
+			// Remove the fake ad from DOM
+			document.body.removeChild(fake_ad);
+		}, 500);
+	});
 })();
